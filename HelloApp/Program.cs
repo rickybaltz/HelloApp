5dec55051b557a55602a28acfe1fea2a72fd0664
Ricardo Baltazar <rickybaltz@gmail.com>
2024-01-11 21:54:40 -0300

    Primeiro commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Imprime um texto na tela.
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("Seja bem-vindo ao meu primeiro programa!");
+
+string nome;
+int idade;
+decimal peso;
+
+Console.WriteLine("Qual é o seu nome?");
+
+// Lê o nome digitado pelo usuário
+nome = Console.ReadLine();
+
+Console.WriteLine("Prazer em conhecer: " + nome + "!");
+
+Console.WriteLine("Qual é a sua idade?");
+
